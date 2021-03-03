ef4d1071e28883fa231049f8b289359261209e13
Eugene <iyao@mail.ru>
2021-03-03 13:30:22 +0300

    first commit

---
+++ app/src/main/java/ru/samsung/itacademy/mdev/getusdrate/RateCheckInteractor.kt
@@ -0,0 +1,27 @@
+package ru.samsung.itacademy.mdev.getusdrate
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+
+class RateCheckInteractor {
+    val networkClient = NetworkClient()
+
+    suspend fun requestRate(): String {
+        //Write your code here:
+        return ""
+    }
+
+    private fun parseRate(jsonString: String): String {
+        try {
+            return JSONObject(jsonString)
+                .getJSONObject("rates")
+                .getJSONObject("USDRUB")
+                .getString("rate")
+        } catch (e: Exception) {
+            Log.e("RateCheckInteractor", "", e)
+        }
+        return ""
+    }
+}
